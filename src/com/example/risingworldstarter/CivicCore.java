b7d1ee6c39723040a27890df9704293d6604ee2e
Adam <adamturbo675@gmail.com>
2026-09-07 14:24:21 -0500

    Set version

--- src/com/example/risingworldstarter/CivicCore.java
+++ src/com/example/risingworldstarter/CivicCore.java
@@ -1199,7 +1199,7 @@ public final class CivicCore extends Plugin implements Listener {
         if (previous != null) player.removeUIElement(previous.window());
 
         String version = getClass().getPackage().getImplementationVersion();
-        if (version == null || version.isBlank()) version = "0.9.0";
+        if (version == null || version.isBlank()) version = "0.9.1";
 
         UIElement window = new UIElement();
         window.setPosition(50f, 50f, true);
